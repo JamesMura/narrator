c13df6e6dc70ec58be10c9273b9b5b9837e7c418
James Muranga <jmured@gmail.com>
2022-08-31 16:57:49 -0700

    Adds devise

--- Gemfile
+++ Gemfile
@@ -71,3 +71,4 @@ group :test do
   gem "webdrivers"
 end
 gem "graphiql-rails", group: :development
+gem 'devise'
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import react from '@vitejs/plugin-react'
+import FullReload from 'vite-plugin-full-reload'
+
 export default defineConfig({
   plugins: [
     RubyPlugin(),
-    react()
+    react(),
+    FullReload(['config/routes.rb', 'app/views/**/*'])
   ],
 })
